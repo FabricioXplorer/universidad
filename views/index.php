bf428d8f7f6b4e73ca3a679a60042c6fd0e9dc23
Teddy_2204 <anttoniogonj56@gmail.com>
2024-10-08 10:02:52 -0400

    estiloResponsi

--- views/index.php
+++ views/index.php
@@ -10,25 +10,25 @@
 <?php include '../includes/navbar.php'; ?>
     <h1>Solicitud de Materia</h1>
 
-
+    <!-- Primer formulario para ingresar el C.I. -->
     <form id="primer-formulario" action="buscarEstudiantes.php" method="POST" enctype="multipart/form-data" onsubmit="return buscarEstudiante(event)">
         <label for="ci">C.I.:</label>
-        <input type="text" id="ci" name="ci" required><br>
+        <input type="text" id="ci" name="ci" required>
         <button type="submit">Solicitar</button>
     </form>
 
-
+    <!-- Segundo formulario para registrar la solicitud de materia -->
     <form id="segundo-formulario" action="solicitudes.php" method="POST" enctype="multipart/form-data" style="display: none;">
         <input type="hidden" id="id_estudiante" name="id_estudiante">
 
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" readonly><br>
+        <input type="text" id="nombre" name="nombre" readonly>
 
         <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="apellido" readonly><br>
+        <input type="text" id="apellido" name="apellido" readonly>
 
         <label for="carrera">Carrera:</label>
-        <input type="text" id="carrera" name="carrera" readonly><br>
+        <input type="text" id="carrera" name="carrera" readonly>
 
         <label for="id_materia">Materia:</label>
         <select id="id_materia" name="id_materia" required>
@@ -45,16 +45,16 @@
 
             $conn->close();
             ?>
-        </select><br>
+        </select>
 
         <label for="estado">Estado:</label>
-        <input type="text" id="estado" name="estado" required><br>
+        <input type="text" id="estado" name="estado" required>
 
         <label for="imagen">Subir Imagen:</label>
-        <input type="file" id="imagen" name="imagen" accept="image/*" required><br>
+        <input type="file" id="imagen" name="imagen" accept="image/*" required>
 
         <label for="fecha">Fecha:</label>
-        <input type="date" id="fecha" name="fecha" required><br>
+        <input type="date" id="fecha" name="fecha" required>
 
         <button type="submit">Registrar</button>
     </form>
@@ -75,7 +75,6 @@
             .then(response => response.json())
             .then(data => {
                 if (data.success) {
-        
                     document.getElementById('id_estudiante').value = data.id_estudiante;
                     document.getElementById('nombre').value = data.nombre;
                     document.getElementById('apellido').value = data.apellido;
